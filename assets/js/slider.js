b7b876bad59da4d08b2c00411f8f5bb79163a353
sixdeathsix <spyro129steam@gmail.com>
2023-03-08 22:16:42 +0300

    commit

--- assets/js/slider.js
+++ assets/js/slider.js
@@ -27,3 +27,30 @@ for (const section of sectionsWithCarousel) {
         }
     });
 }
+
+const about = document.getElementById('about');
+const promo = document.getElementById('promo');
+const healthcenter = document.getElementById('health-center');
+const attractions = document.getElementById('attractions');
+const tours = document.getElementById('tours');
+const happycenter = document.getElementById('happy-center');
+const cafe = document.getElementById('cafe');
+
+// Create the observer, same as before:
+const observer = new IntersectionObserver(entries => {
+    entries.forEach(entry => {
+        if (entry.isIntersecting) {
+            entry.target.classList.add('animate__fadeInUp');
+            entry.target.classList.add('animate__animated');
+            return;
+        }
+    });
+});
+
+observer.observe(about);
+observer.observe(promo);
+observer.observe(healthcenter);
+observer.observe(attractions);
+observer.observe(tours);
+observer.observe(happycenter);
+observer.observe(cafe);
